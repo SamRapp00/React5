53c0e5e9e100b2bcd475882dee9d899472f0349f
Sam Christen <samchristenlds@gmail.com>
2024-07-02 18:57:21 -0600

    inital commit

---
+++ src/redux/store.js
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import potentialCountriesReducer from "../redux/slices/potentialCountriesSlice";
+import displayedCountryReducer from "../redux/slices/displayCountrySlice";
+
+
+
+
+
+export default configureStore({
+    reducer: {
+        potentialCountries: potentialCountriesReducer,
+        displayedCountry: displayedCountryReducer,
+    },
+});
+
